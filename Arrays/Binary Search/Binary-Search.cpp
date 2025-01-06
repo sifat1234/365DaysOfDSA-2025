84a78a7d49a903323ceb3586b759afb7dc27e030
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-06 19:12:14 +0600

    Binary Search

---
+++ Arrays/Binary Search/Binary-Search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        int mid;
+        
+
+        while(start<=end){
+            mid=start+(end-start)/2;
+
+            if(nums[mid]==target){
+                return mid;
+                break;
+            }
+            if(nums[mid]<target){
+                start=mid+1;
+            }
+            if(nums[mid]>target){
+                end=mid-1;
+            }
+        }
+        return -1;
+
+    }
+};
