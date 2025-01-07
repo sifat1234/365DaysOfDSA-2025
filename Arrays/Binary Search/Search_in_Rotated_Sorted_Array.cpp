71597cf1f2a4be37c88fc01ff13b452db23c1768
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-07 17:44:47 +0600

    Search in Rotated Sorted Array

---
+++ Arrays/Binary Search/Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        int mid;
+        while(start<=end){
+            mid=start+(end-start)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+
+           else if(nums[mid]>=nums[0]){
+                if(nums[start]<=target && nums[mid]>target){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+
+            else{
+                if(nums[mid]<=target && nums[end]>=target){
+                   start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+
+            
+            
+        }
+        return -1;
+    }
+};
