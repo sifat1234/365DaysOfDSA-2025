c9647be38e408db85141b6d3fcfcea5397ec2b14
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-12 21:15:27 +0600

    Two Sum II - Input Array Is Sorted

---
+++ Arrays/Two Pointer/Two Sum II - Input_Array_Is_Sorted.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+    //2 pointer 
+     int low = 0;
+        int high = numbers.size() - 1;
+        while(low <= high) {
+            if(numbers[low] + numbers[high] == target) return {low + 1, high + 1};
+            else if(numbers[low] + numbers[high] < target) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return {};
+
+
+    }
+
+};
