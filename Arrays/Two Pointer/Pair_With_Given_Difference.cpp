b2f1bbc2597be8fa8f4033908c7524022bccb2ee
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-12 22:49:29 +0600

    Pair With Given Difference

--- Arrays/Two Pointer/Pair_With_Given_Difference.cpp
+++ Arrays/Two Pointer/Pair_With_Given_Difference.cpp
@@ -3,6 +3,10 @@ int solve(int* A, int n1, int B) {
     sort(A.begin(), A.end());
     int start= 0;
     int end= 1;
+
+    if(B<0){
+      B= B*-1;
+    }
     
     while(end<n1){
         if(A[end]-A[start]==B){
