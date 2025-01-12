5798ef4eeb2912eba6e4f191de7f766c2ec4ca0c
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-12 21:30:13 +0600

    Two Sum II - Input Array Is Sorted

---
+++ Arrays/Binary Search/Two Sum II - Input_Array_Is_Sorted.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    //TC:-O(NLOGN),SC:-O(1)
+    int search(int target,vector<int>&nums,int left,int right)
+    {
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            if(nums[mid]>target)
+            {
+                right=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    vector<int> twoSum(vector<int>&nums,int target) 
+    {
+        for(int i=0;i<nums.size();i++)
+        {
+            int j=search(target-nums[i],nums,i+1,nums.size()-1);
+            if(j!=(-1))
+            {
+                return {i+1,j+1};
+            }
+        }
+        return {-1,-1};
+    }
+};
