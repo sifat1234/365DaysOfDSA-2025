8fbb50d9ce5a0f40c63bb4ad627df90cbdabf33b
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-13 19:09:59 +0600

    Kadane's Algorithm

---
+++ Arrays/6. some array questions/kadanes_algorithm.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here..
+         long long maxi = INT_MIN, prefix = 0;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            prefix += arr[i];
+            maxi = max(prefix, maxi);
+
+            if (prefix < 0)
+                prefix = 0;
+        }
+
+        return maxi;
+    
+    }
+};
