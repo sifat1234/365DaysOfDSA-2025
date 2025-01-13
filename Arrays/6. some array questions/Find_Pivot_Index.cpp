aab5b3c151b360c080c9065d45e6132deaad507b
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-13 20:09:18 +0600

    Find Pivot Index

---
+++ Arrays/6. some array questions/Find_Pivot_Index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int leftSum=0 ;
+        int n=nums.size();
+        int rightSum = accumulate(nums.begin(), nums.end(), 0) ;
+
+        for(int i = 0 ; i<n; i++){
+            rightSum-=nums[i];
+            
+
+            if(leftSum == rightSum){
+               return i;
+            }
+                 leftSum+=nums[i]; 
+           
+        }
+        return -1;
+    }
+};
