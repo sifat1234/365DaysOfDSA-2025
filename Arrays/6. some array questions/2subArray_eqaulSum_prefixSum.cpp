d058e8c7fa0c8981bc4f9d3b59f9faf8507de321
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-13 18:35:44 +0600

    2 subarray eqaul sum using prefixSum

---
+++ Arrays/6. some array questions/2subArray_eqaulSum_prefixSum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool divide(const vector<int> &v)
+{
+    int prefix = 0, total_sum = 0, n = v.size();
+
+    for (int i = 0; i < n; i++)
+        total_sum += v[i];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        prefix += v[i];
+        int ans= total_sum-prefix;
+        
+        if (ans == prefix){
+          return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    vector<int> v(n);
+    cout << "Enter the element in array: ";
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    if (divide(v))
+        cout << "Equal sum possible";
+    else
+        cout << "Equal sum not possible";
+    return 0;
+}
